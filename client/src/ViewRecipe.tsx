6ff9ef681bde30682a47e0ac529918ff127a1ab6
pthakur11205 <pthakur@uw.edu>
2024-06-26 22:07:03 -0400

    ViewRecipe css

--- client/src/ViewRecipe.tsx
+++ client/src/ViewRecipe.tsx
@@ -1,6 +1,7 @@
 // A page that displays the recipe in a more organized manner
 import React from 'react';
 import { Recipe } from './recipe';
+import './ViewRecipe.css'
 
 type ViewRecipeProps = {
   recipe: Recipe;
@@ -9,26 +10,26 @@ type ViewRecipeProps = {
 
 
 export const ViewRecipe: React.FC<ViewRecipeProps> = ({ recipe, onBackClick }) => {
-  return (
-    <div>
-      <h2>{recipe.name}</h2>
-      <p><strong>Food Type:</strong> {recipe.foodType}</p>
-      <p><strong>Prep Time:</strong> {recipe.prepTime} minutes</p>
-      <h4>Ingredients:</h4>
-      <ul>
-        {recipe.ingredients.map((ingredient, idx) => (
-          <li key={idx}>
-            {ingredient.name}: {ingredient.quantity}
-          </li>
-        ))}
-      </ul>
-      <h4>Instructions:</h4>
-      <ol>
-        {recipe.instructions.map((instruction, idx) => (
-          <li key={idx}>{instruction}</li>
-        ))}
-      </ol>
-      <button onClick={onBackClick}>Back to List</button>
-    </div>
-  );
-};
+    return (
+      <div className="view-recipe-container">
+        <h2>{recipe.name}</h2>
+        <p><strong>Food Type:</strong> {recipe.foodType}</p>
+        <p><strong>Prep Time:</strong> {recipe.prepTime} minutes</p>
+        <h4>Ingredients:</h4>
+        <ul>
+          {recipe.ingredients.map((ingredient, idx) => (
+            <li key={idx}>
+              {ingredient.name}: {ingredient.quantity}
+            </li>
+          ))}
+        </ul>
+        <h4>Instructions:</h4>
+        <ol>
+          {recipe.instructions.map((instruction, idx) => (
+            <li key={idx}>{instruction}</li>
+          ))}
+        </ol>
+        <button onClick={onBackClick}>Back to List</button>
+      </div>
+    );
+  };
